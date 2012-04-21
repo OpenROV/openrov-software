d162556706723065bc95e31c7e69d9dfd3bafbd8
Bran Sorem <bransorem@pocket.(none)>
2012-04-21 12:32:30 -0700

    Add support for Arduino control over serial, commented out

--- src/OpenROV.js
+++ src/OpenROV.js
@@ -1,7 +1,7 @@
 /* 
  * Created for OpenROV:  www.openrov.com
  * Author:  Bran Sorem (www.bransorem.com)
- * Date: 03/19/12
+ * Date: 04/21/12
  * 
  * Description:
  * This script creates a directory and sends that as an argument to a spawned process (capture.cpp).
@@ -18,26 +18,26 @@
  * letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
  *
  */
-
+ 
 var socket = require('socket.io'), 
   spawn = require('child_process').spawn, 
   sys = require('util'), 
   events = require('events'), 
   fs = require('fs'), 
   path = require('path');
-
+ 
 // Create dir (current date) to save images into
 var time = new Date();
 var location = time.getFullYear() + 
     '-' + time.getMonth() + 
     '-' + time.getDate() + '/';
-
+ 
 // Open capture app as a child process
 var cmd = './capture';  // rename to correspond with your C++ compilation
 var capture_process = spawn(cmd, [location]);
 var default_opts = { device : '/dev/video0' };
-
-
+ 
+ 
 /* =====================================
    =========== OpenROV class ===========
    ===================================== */
@@ -48,12 +48,12 @@ var OpenROV = function (options){
   return this;
 };
 sys.inherits(OpenROV, events.EventEmitter);
-
+ 
 // ============ init() ============
 // create directory to store photos
 // ================================
 OpenROV.prototype.init = function(){ fs.mkdir(location, 0755); };
-
+ 
 // ============ reset() ===============
 // reset the device to default settings
 // ====================================
@@ -61,7 +61,7 @@ OpenROV.prototype.reset = function (){
   this.opts = default_opts;
   return default_opts;
 };
-
+ 
 // ============ update(options) ============
 // options (obj)
 // update options of rov instance
@@ -70,7 +70,7 @@ OpenROV.prototype.update = function (options){
   this.opts = mixin(options, this.opts)
   return this.opts;
 };
-
+ 
 // ============ close() =============
 // close the camera and child process
 // ==================================
@@ -78,27 +78,27 @@ OpenROV.prototype.close = function(){
   capture_process.stdin.write('exit');
   process.kill(capture_process.pid, 'SIGHUP');
 };
-
+ 
 // ============ capture(options, callback) ============
 // options (obj) - options for controlling camera
 // callback (function) - callback function
 // Captures a frame from camera and emits it as 'frame'
 // ====================================================
 OpenROV.prototype.capture = function (options, callback){
-
+ 
   this.opts = mixin(options, this.opts);
   var rov = this;
-
+ 
   // when ./capture responds, image has been saved
   capture_process.stdout.on('data', function(response){
-
+ 
     try {
       // remove any trailing newline chars
       var file = response.toString().replace(/(\r\n|\n|\r)/gm, '');
       var imgFile = location + file;
-      
+       
       console.log('Sending: ' + file);
-    
+     
       // open file from system, then emit to server
       fs.readFile(imgFile, 'base64', function(err, img){
         if (!err) rov.emit('frame', img);
@@ -107,25 +107,25 @@ OpenROV.prototype.capture = function (options, callback){
     }
     catch (captureError){ console.log(captureError); }
   });
-
+ 
   // this function loops, 'grabs' frame from camera (sends stdin to child process ./capture)
   var grab = function (){ 
     try{
-      
+       
       var self = this;
       self.emitter = this.emitter;
       self.arguments = [];
-      self.format = ".jpg";
+//      self.format = ".jpg";
       format = ".jpg";
-  
+   
       path.exists(this.opts.device, function (exists){
-  
+   
         // only call this function if camera is connected
         var alive = function (){
           var file = getTime() + format + '\n\r';
           capture_process.stdin.write(file);
         }
-    
+     
         // uh-oh, no camera connected!
         if (!exists){
           var message = 'no device (' + self.opts.device + ').';
@@ -137,11 +137,11 @@ OpenROV.prototype.capture = function (options, callback){
           return;
         }
         alive.apply(self);
-  
+   
       });
     } catch (capError){ console.write(capError); }
   };
-
+ 
   // loop based on delay (if set) in milliseconds
   if (this.opts.delay){
     setInterval(function (self){
@@ -149,9 +149,9 @@ OpenROV.prototype.capture = function (options, callback){
     }, this.opts.delay, this);
   } else { grab.apply(this); }
 };
-
-
-
+ 
+ 
+ 
 // ================================================
 // get current time in image name format
 function getTime(){
@@ -164,14 +164,14 @@ function getTime(){
   if (min < 10) min = '0' + min;
   if (sec < 10) sec = '0' + sec;
   if (millisec < 100){
-    if (millisec.length < 10){ millisec = '0' + millisec; }
+    if (millisec < 10){ millisec = '0' + millisec; }
     millisec = '0' + millisec;
   }
   var id = hrs + '.' + min + '.' + sec + '.' + millisec;
-  
+   
   return id;
 }
-
+ 
 // ============ mixin (source, destination) ============
 // -source (obj)
 // -destination (obj)
@@ -182,7 +182,7 @@ function getTime(){
 //    https://github.com/pdeschen/camelot
 // =====================================================
 var mixin = function (source, destination){
-
+ 
   if (typeof (source) == "object"){
     for ( var prop in source){
       if ((typeof (source[prop]) == "object") && (source[prop] instanceof Array)){
@@ -209,9 +209,8 @@ var mixin = function (source, destination){
       }
     }
   }
-
+ 
   return destination;
 };
-
+ 
 module.exports = OpenROV;
-
