0aad7b4e3faf527cad2b13ddc14758088398cb20
Simon Murtha-Smith <simon@murtha-smith.com>
2012-04-21 21:41:58 -0700

    add keypad as backup if gamepad is not attached

--- src/static/js/gamepad.js
+++ src/static/js/gamepad.js
@@ -1,18 +1,27 @@
 var GamePad = function() {
-  var isChrome = navigator.webkitGamepads !== undefined;
-  var controller;
   var gp = {};
 
-  if (isChrome) {
-    controller = navigator.webkitGamepads[0];
-  }
-
-  gp.getButtons = function() {
-    return navigator && navigator.webkitGamepads && navigator.webkitGamepads[0] && navigator.webkitGamepads[0].buttons;
-  }
-
-  gp.getAxes = function() {
+  var getAxes = function() {
     return navigator && navigator.webkitGamepads && navigator.webkitGamepads[0] && navigator.webkitGamepads[0].axes;
+  }
+
+  gp.getPositions = function() {
+    var axes = getAxes();
+    var positions = {
+      throttle: 0,
+      yaw: 0,
+      lift: 0
+    };
+    if(!axes) return positions;
+    return {
+      throttle: -1*axes[1],
+      yaw: axes[0],
+      lift: -1*axes[3]
+    };
+  }
+
+  gp.isAvailable = function() {
+    return !!(navigator && navigator.webkitGamepads && navigator.webkitGamepads[0] && navigator.webkitGamepads[0].axes);
   }
 
   return gp;
