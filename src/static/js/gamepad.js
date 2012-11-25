e848b56bff75f4c3019736157a2cf9131b3dbe70
Simone Chiaretta <simone@piyosailing.com>
2012-11-25 20:38:29 +0100

    First part of issue #40: updated gamepad code to latest version of chrome

--- src/static/js/gamepad.js
+++ src/static/js/gamepad.js
@@ -1,9 +1,38 @@
 var GamePad = function() {
   var gp = {};
 
-  var getAxes = function() {
-    return navigator && navigator.webkitGamepads && navigator.webkitGamepads[0] && navigator.webkitGamepads[0].axes;
-  }
+    if ( !window.requestAnimationFrame ) {
+        window.requestAnimationFrame = ( function() {
+            return window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+                    window.setTimeout( callback, 1000 / 60 );
+                };
+        })();
+    }
+
+    var getPadsFunction = function () {
+        return navigator.webkitGetGamepads || navigator.mozGetGamepads || navigator.getGamepads;
+    };
+
+    var getPadsField = function() {
+        return navigator.webkitGamepads || navigator.mozGamepads || navigator.gamepads;
+    };
+
+    var isSupported = function () {
+        return ((getPadsField() != undefined) || (getPadsFunction() != undefined));
+    };
+
+    var getPads = function () {
+        var getRawPads = getPadsFunction();
+        var rawPads = getPadsField();
+        if ((!rawPads) && (getRawPads)) {
+            rawPads = getRawPads.call(navigator)
+        }
+        return rawPads;
+    }
 
     var servoTiltHandler = function(value){};
     var brightnessHandler = function(value){};
@@ -16,22 +45,32 @@ var GamePad = function() {
     };
 
     gp.getPositions = function() {
-    var axes = getAxes();
-    var positions = {
-      throttle: 0,
-      yaw: 0,
-      lift: 0
-    };
-    if(!axes) return positions;
-    return {
-      throttle: -1*axes[1],
-      yaw: axes[0],
-      lift: -1*axes[3]
-    };
+    window.requestAnimationFrame(gp.getPositions);
+
+    var pad = getPads()[0];
+        if(pad){
+            var axes = pad.axes;
+            var positions = {
+                throttle: 0,
+                yaw: 0,
+                lift: 0
+            };
+            if(!axes) return positions;
+            return {
+                throttle: -1*axes[1],
+                yaw: axes[0],
+                lift: -1*axes[3]
+            };
+        }
   }
 
   gp.isAvailable = function() {
-    return !!(navigator && navigator.webkitGamepads && navigator.webkitGamepads[0] && navigator.webkitGamepads[0].axes);
+      if(isSupported())
+        window.requestAnimationFrame(gp.getPositions);
+        if(getPads()[0]!=undefined){
+            return true;
+        }
+      return false;
   }
 
   return gp;
