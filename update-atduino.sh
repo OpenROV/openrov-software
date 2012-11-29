78421b010125918d0a24001b2a705b3d35ac0b83
Dominik Fretz <info@codewithpassion.com>
2012-11-29 06:54:30 +0100

    Adding update-arduino.sh script for easy commandline update

--- update-atduino.sh
+++ update-atduino.sh
@@ -5,5 +5,13 @@ DIR="`dirname \"$0\"`"
 cd $DIR
 DIR=`pwd`
 
+echo Stopping OpenROV Cockpit
+/etc/init.d/openrov stop
+
 tar zcf $TEMPDIR/openrov.tar.gz $DIR/arduino/OpenROV
+
+echo Installing firmware
 sudo $DIR/linux/arduino/firmware-install.sh $TEMPDIR/openrov.tar.gz
+
+echo starting OpenROV Cockpit
+/etc/init.d/openrov start
