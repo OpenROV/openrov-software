fcce942f4c0a01e756c99a9886c0190a7fcd163d
BrianAdams <brian@babastik.com>
2013-03-01 08:22:03 +0100

    Added a current read-out. Smoothed the current sampling in the ardurino. Added beagle CPU utilization.

--- arduino/OpenROV/OpenROV.ino
+++ arduino/OpenROV/OpenROV.ino
@@ -17,6 +17,16 @@ Servo tilt;
 // array[0] will be the number of arguments in the command
 int array[MAX_ARGS];
 
+// Define the number of samples to keep track of.  The higher the number,
+// the more the readings will be smoothed, but the slower the output will
+// respond to the input.  Using a constant rather than a normal variable lets
+// use this value to determine the size of the readings array.
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int index = 0;                  // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
 
 void setup(){
 
@@ -29,6 +39,10 @@ void setup(){
   motors.reset();
   
   time.reset();
+
+// initialize all the readings to 0: 
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;     
 }
 
 void loop(){
@@ -62,10 +76,30 @@ void loop(){
     }
   }
 
+  if (time.elapsed (100)) {
+    // subtract the last reading:
+    total= total - readings[index];         
+    // read from the sensor:  
+    readings[index] = iout.read();
+    delay(1); 
+    // add the reading to the total:
+    total= total + readings[index];       
+    // advance to the next position in the array:  
+    index = index + 1;                    
+
+    // if we're at the end of the array...
+    if (index >= numReadings)              
+      // ...wrap around to the beginning: 
+      index = 0;                           
+
+    // calculate the average:
+    average = total / numReadings;
+  }        
+
   // send voltage and current
   if (time.elapsed(1000)) {
     vout.send(vout.read());
-    iout.send(iout.read());
+    iout.send(average);
     Serial.print("time:");
     Serial.println(millis());
   }
