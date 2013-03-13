63bd04e524c75113330cf6a1389cd86f099d753c
BrianAdams <brian@babastik.com>
2013-03-13 09:13:16 +0100

    fixed #82; Added additional keyboard commands for better thrust mgt. Using the trim you can litteraly inch the motors up 1/1000th of a unit

--- src/static/js/keypad.js
+++ src/static/js/keypad.js
@@ -1,4 +1,7 @@
 var KEYS = {
+  32: { // space (all-stop)
+    command: 'stop'
+  },
   38: { // up (forward)
     command: 'command',
     position: 'throttle',
@@ -29,6 +32,34 @@ var KEYS = {
     position: 'lift',
     value: -1
   },
+  49: { //1 (power-1)
+    command: 'power',
+    value: .1
+  },
+  50: { //2 (power-2)
+    command: 'power',
+    value: .25 
+  },
+  51: { //3 (power-3)
+    command: 'power',
+    value: .5
+  },
+  52: { //4 (power-4)
+    command: 'power',
+    value: .75
+  },
+  53: { //5 (power-5)
+    command: 'power',
+    value: 1
+  },
+  219: { //[ (vtrim -)
+    command: 'vtrim',
+    value: -1
+  },
+  221: { //] (vtrim +) 
+    command: 'vtrim',
+    value: 1
+  }, 
   81: { //Q (tilt up)
     command: 'tilt',
     value: 1
@@ -41,17 +72,19 @@ var KEYS = {
     command: 'tilt',
     value: -1
   },
-  187: { //+ (brightness up)
+  187: { //+ (brightness up) - Firefox and Opera not compatible
      command: 'light',
      value: 1
   },
-  189: { //- (brightness down)
+  189: { //- (brightness down) - Firefox and Opera not compatible
      command: 'light',
      value: -1
   }
 }
 
 var KeyPad = function() {
+  var power = .5; //default to mid power
+  var vtrim = 0; //default to no trim
   var kp = {};
   var servoTiltHandler = function(value){};
   var brightnessHandler = function(value){};
@@ -64,6 +97,18 @@ var KeyPad = function() {
         brightnessHandler=callback;
     };
 
+  var vtrimHandler = function(value){
+    vtrim+=value;
+    positions.throttle = (1/1000)*vtrim;
+  };
+
+  var stopHandler = function(){
+    vtrim = 0;
+    positions.throttle = 0;
+    positions.yaw = 0;
+    positions.lift = 0;
+  };
+
   var positions = {
     throttle: 0,
     yaw: 0,
@@ -75,19 +120,28 @@ var KeyPad = function() {
     if (!info) return;
     evt.preventDefault();
     if(info.command=='command')
-        positions[info.position] = info.value;
+        positions[info.position] = info.value*power;
     else if(info.command=='tilt')
         servoTiltHandler(info.value);
     else if(info.command=='light')
         brightnessHandler(info.value);
+    else if(info.command=='power')
+        power=info.value;
+    else if(info.command=='vtrim')
+	vtrimHandler(info.value);
+    else if(info.command=='stop')
+	stopHandler();
   });
 
   $(window).keyup(function(evt) {
     var info = KEYS[evt.keyCode];
     if (!info) return;
     evt.preventDefault();
-    if(info.command=='command')
+    if(info.command=='command'){
        positions[info.position] = 0;
+       if (info.position == 'throttle') 
+         positions.throttle = (1/1000)*vtrim;
+    }
     else if(info.command=='tilt')
         servoTiltHandler(info.value);
   });
