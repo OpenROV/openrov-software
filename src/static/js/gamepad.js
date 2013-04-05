97f4d2ed91474cf36fea1dbb1838a59e534b47ea
BrianAdams <brian@babastik.com>
2013-04-05 06:53:42 +0200

    Added trim and power selection options to the gamepad. Only tested on the Logitech F310.  Did not change any existing mappings so it should not break anything else.

--- src/static/js/gamepad.js
+++ src/static/js/gamepad.js
@@ -1,3 +1,5 @@
+//http://www.html5rocks.com/en/tutorials/doodles/gamepad/gamepad-tester/tester.html
+//useful for testing the buttons and finding the numbers
 var GamePad = function() {
   var gp = {};
   
@@ -78,6 +80,50 @@ var GamePad = function() {
         if(buttons[0]==1)
             servoTiltHandler(-1);
     };
+    
+    var powerLevel = .4;
+    var updatePowerLevels = function(buttons){
+      
+        if (buttons[8]==1) //Start {
+          powerLevel+= .2;
+        if (powerLevel > 1) powerLevel = .2;
+        
+    };
+
+    var ttrim = 0;
+    var ltrim = 0;
+    var buttonregisterd = -1;
+    var axesregisterd = -1;
+    var updateTrim = function(buttons,axes){
+
+        if(buttons[5]==1 && buttonregisterd ==-1){ //right top sholder
+            ttrim++;;
+            buttonregisterd = 5;
+        }
+        if(axes[5]==1 && axesregisterd ==-1){ //right lower sholder
+            ttrim--;
+            axesregisterd = 5;
+        }
+        if(buttons[4]==1 && buttonregisterd ==-1){ //left top sholder
+            ltrim++;
+            buttonregisterd = 4;
+        }
+        if(axes[2]==1 && axesregisterd ==-1){ //left lower sholder
+            ltrim--;
+            axesregisterd = 2;
+        }
+        if(buttons[6]==1 && buttonregisterd ==-1){ //first stick button
+            ttrim=0;
+            buttonregisterd = 6;
+        }
+        if(buttons[7]==1 && buttonregisterd ==-1){ //2nd stick button
+            ltrim=0;
+            buttonregisterd = 7;
+        }
+        if (buttonregisterd>-1 && buttons[buttonregisterd] == 0) buttonregisterd = -1;
+        if (axesregisterd>-1 && axes[axesregisterd] == -1) axesregisterd = -1;
+
+    };
 
     gp.getPositions = function() {
         window.requestAnimationFrame(updateStatus);
@@ -100,17 +146,25 @@ var GamePad = function() {
             {
                 updateLightStatus(buttons);
                 updateTiltStatus(buttons);
+                updatePowerLevels(buttons);
+                updateTrim(buttons,axes);
             }
             if(axes) {
+                updateTrim(buttons,axes);
                 positions = {
-                    throttle: -1*axes[1],
-                    yaw: axes[0],
-                    lift: -1*axes[3]
+                    throttle: -1*axes[1]*powerLevel,
+                    yaw: axes[0]*powerLevel,
+                    lift: -1*axes[3]*powerLevel
                 };
                 if (positions.throttle > -deadzone && positions.throttle < deadzone) positions.throttle = 0;
                 if (positions.yaw > -deadzone && positions.yaw < deadzone) positions.yaw = 0;
                 if (positions.lift > -deadzone && positions.lift < deadzone) positions.lift = 0;
+                if (positions.throttle ==0) {
+                  positions.throttle = (1/1000)*ttrim;
+                }
+                if (positions.lift ==0) positions.lift = (1/1000)*ltrim;
             }
+            
             padStatus.position = positions;
         }
   }
