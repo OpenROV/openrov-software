dcb24b9161f490323a637070bb96c3a10dfba7ad
Philip Mathieu <philip.eng.mathieu@gmail.com>
2013-05-08 10:29:27 -0300

    Update config.js
    Add boolean to skip setting up UART in OpenROVController.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -26,6 +26,7 @@ module.exports = {
   debug:            process.env.NODE_DEBUG      !== 'false',
   debug_commands:   false, //process.env.NODE_DEBUG_COMMANDS     === 'true',
   production:       process.env.NODE_ENV         || true,
+  setuart:          true,
   sample_freq:     (process.env.SAMPLE_FREQ      && parseInt(process.env.SAMPLE_FREQ))     || 20, //Hz
   dead_zone:        process.env.DEAD_ZONE        && parseInt(process.env.DEAD_ZONE)        || 10,
   video_frame_rate: process.env.VIDEO_FRAME_RATE && parseInt(process.env.VIDEO_FRAME_RATE) || 15,
