65136f176c70e24b4a5a6266f637b31b92432bbe
BrianAdams <brian@babastik.com>
2013-08-16 06:58:04 +0200

    Fixed #73; Fixed the timing issues from the reset to fix UART based firmware updates of the ATMEGA328p

--- linux/arduino/firmware-upload.sh
+++ linux/arduino/firmware-upload.sh
@@ -22,24 +22,29 @@ cd $1  || error_exit "$LINENO:"
 echo Setting up uploader 1>&2
 echo Serial port settings 1>&2
 /opt/openrov/linux/setuart.sh 1>&2 || error_exit "$LINENO:"
-/opt/openrov/linux/reset.sh 1>&2
+#/opt/openrov/linux/reset.sh 1>&2
 #ino upload -m atmega328 -p /dev/ttyO1 1>&2 
 
 COUNTER=0
 OUTPUT=`ino upload -p /dev/ttyO1 2>&1`
-while [ $COUNTER -lt 5 ]; do 
-	/opt/openrov/linux/reset.sh 1>&2
-	sleep 0.4
-        echo $OUTPUT  |  grep "bytes of flash verified"
+#OUTPUT=`avrdude -P /dev/ttyO1 -c arduino -D -vvvv -p m328p -U flash:w: .build/uno/firmware.hex 2>&1`
+while [ $COUNTER -lt 9 ]; do
+        echo $COUNTER
+        (sleep 0.0$COUNTER && /opt/openrov/linux/reset.sh) & 1>&2
+        avrdude -P /dev/ttyO1 -c arduino -D -v -p m328p -U flash:w:.build/uno/firmware.hex 2>&1
+
+#	/opt/openrov/linux/reset.sh 1>&2
+#	sleep 0.4
+#        echo $OUTPUT  |  grep "bytes of flash verified"
 	if [ $? -eq 0 ] 
 		then
 			echo upload successfull! 1>&2
-			echo $OUTPUT 1>&2
+			#echo $OUTPUT 1>&2
 			exit 0
 		fi
 	COUNTER=`expr $COUNTER + 1`
 	echo upload failed, trying again. 1>&2
-	echo $OUTPUT 1>&2
-	sleep 1
+#	echo $OUTPUT 1>&2
+#	sleep 1
 done
 error_exit "Failed to upload after numerous tries. Aborting."
