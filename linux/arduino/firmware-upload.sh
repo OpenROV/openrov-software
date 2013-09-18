8dadbda27b53c07ca886634c919f7c714b1af7dc
BrianAdams <brian@Babs.local>
2013-09-18 02:02:37 -0700

    Switched to SPI programming and enabled firmware erase cycle

--- linux/arduino/firmware-upload.sh
+++ linux/arduino/firmware-upload.sh
@@ -29,7 +29,7 @@ OUTPUT=`ino upload -p /dev/ttyO1 2>&1`
 while [ $COUNTER -lt 9 ]; do
         echo $COUNTER
         (sleep 0.0$COUNTER && /opt/openrov/linux/reset.sh) & 1>&2
-        avrdude -P /dev/ttyO1  -c arduino -D -vv -p m2560 -U flash:w:.build/mega2560/firmware.hex 2>&1
+        avrdude -P /dev/spidev1.0 -c linuxspi -vv -p m2560 -U flash:w:.build/mega2560/firmware.hex 2>&1
 
 #	/opt/openrov/linux/reset.sh 1>&2
 #	sleep 0.4
