c0ce7fd066e5e2b1d949f6cee2545be2ae61b767
Dominik Fretz <dominik.fretz@gmail.com>
2013-10-09 23:19:26 +1100

    Moving serial handling to own class

---
+++ src/lib/Hardware.js
@@ -0,0 +1,43 @@
+var serialPort = require('serialport')
+  , EventEmitter = require('events').EventEmitter
+  , StatusReader = require('./StatusReader')
+  , CONFIG = require('./config');
+
+function Hardware() {
+	var hardware = new EventEmitter();
+	var reader = new StatusReader();
+	var hardware.serial = { };
+
+	hardware.connect = function() {
+		hardware.serial = new serialPort.SerialPort(
+		CONFIG.serial, 
+		{
+			baudrate: CONFIG.serial_baud,
+			parser: serialPort.parsers.readline("\r\n")
+		});
+        
+        hardware.serial.on( 'close', function (data) {
+         logger.log('!Serial port closed');
+        });
+
+		hardware.serial.on( "data", function( data ) {
+			var status = reader.parseStatus(data,controller);
+			hardware.emit('status', status);
+		});
+	};
+
+	hardware.write = function(command) {
+		logger.command(command);
+		
+		if(CONFIG.production) {
+			hardware.serial.write(command);
+		}
+	};
+
+	hardware.close = function() {
+		hardware.serial.close();
+	};
+	return hardware;
+};
+
+module.exports = Hardware;
