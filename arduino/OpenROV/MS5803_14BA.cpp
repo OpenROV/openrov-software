fb8e9f849c8af96ea67365b0d81f53f5b3703c68
BrianAdams <brian@Babs.local>
2013-10-16 21:17:08 -0700

    Swapped the MS5803 to Walt's library

--- arduino/OpenROV/MS5803_14BA.cpp
+++ arduino/OpenROV/MS5803_14BA.cpp
@@ -2,350 +2,213 @@
 #if(HAS_MS5803_14BA)
 
 #include "Device.h"
-#include "Pin.h"
 #include "MS5803_14BA.h"
 #include "Settings.h"
 #include "Timer.h"
+#include <Wire.h>
 
-MS5803 sensor = MS5803();
-Timer statustime;
+/*
+Sketch to read a MS5803-14BA pressure sensor, written from scratch.
+Will output data to the serial console.
+
+Written by Walt Holm 
+Initial revision 10 Oct 2013
+Rev 1 12 Oct 2013 -- Implements 2nd order temperature compensation
+*/
+
+
+
+const int DevAddress = MS5803_14BA_I2C_ADDRESS;  // 7-bit I2C address of the MS5803
+
+// Here are the commands that can be sent to the 5803
+// Page 6 of the data sheet
+const byte Reset = 0x1E;
+const byte D1_256 = 0x40; 
+const byte D1_512 = 0x42;
+const byte D1_1024 = 0x44;
+const byte D1_2048 = 0x46;
+const byte D1_4096 = 0x48;
+const byte D2_256 = 0x50;
+const byte D2_512 = 0x52;
+const byte D2_1024 = 0x54;
+const byte D2_2048 = 0x56;
+const byte D2_4096 = 0x58;
+const byte AdcRead = 0x00;
+const byte PromBaseAddress = 0xA0;
+
+//io_timeout is the max wait time in ms for I2C requests to complete
+const int io_timeout = 20;
+
+
+unsigned int CalConstant[8];  // Matrix for holding calibration constants
+
+long AdcTemperature, AdcPressure;  // Holds raw ADC data for temperature and pressure
+float Temperature, Pressure, TempDifference, Offset, Sensitivity;
+float T2, Off2, Sens2;  // Offsets for second-order temperature computation
+Timer DepthSensorSamples;  
+byte ByteHigh, ByteMiddle, ByteLow;  // Variables for I2C reads
+
+// Program initialization starts here
+
+void sendCommand(byte command){
+  Wire.beginTransmission(DevAddress);
+  Wire.write(command);
+  Wire.endTransmission();
+}
 
 void MS5803_14BA::device_setup(){
   Settings::capability_bitarray |= (1 << DEAPTH_CAPABLE);
-  // Initalize the sensor which resets the sensor, downloads the needed coeffecients, 
-  // and does a CRC check on the returned data. This will verify that we are talking to
-  // the device and that all is well.
-  if ( sensor.initalizeSensor() ) {
-    Serial.println( "MS5803:Sensor CRC check OK.;" );
-    sensor.resetSensor();
-  } 
-  else {
-    Serial.println( "MS5803:Sensor CRC check FAILED! There is something wrong!;" );
+
+    log("Depth Sensor setup.");
+  Wire.begin();
+  log("Depth Sensor: initialized I2C");
+  delay(10);
+    
+  // Reset the device and check for device presence
+  
+  sendCommand(Reset);
+  delay(10);
+  log("Depth Sensor is reset");
+   
+  // Get the calibration constants and store in array
+  
+  for (byte i = 0; i < 8; i++)
+  {
+    sendCommand(PromBaseAddress + (2*i));
+    Wire.requestFrom(DevAddress, 2);
+    while(Wire.available()){
+      ByteHigh = Wire.read();
+      ByteLow = Wire.read();
+    }
+    CalConstant[i] = (((unsigned int)ByteHigh << 8) + ByteLow);
   }
   
-  statustime.reset();
+  log("Depth: Calibration constants are:");
+  
+  for (byte i=0; i < 8; i++)
+  {
+    log(CalConstant[i]);
+  }
+  
+  DepthSensorSamples.reset();
+  
 }
-float baseline = 0;
-float ticks=0;
-float temp;
+
 void MS5803_14BA::device_loop(Command command){
-  sensor.readSensor();
+  if (DepthSensorSamples.elapsed(1000)){
+  // Read the Device for the ADC Temperature and Pressure values
   
-  temp = sensor.pressure();
-  temp = abs(temp);
-  if (baseline==0) baseline = temp;
+  sendCommand(D1_512);
+  delay(10);
+  sendCommand(AdcRead);
+  Wire.requestFrom(DevAddress, 3);
   
-  ticks = baseline - temp;
-//  Serial.print(temp);
-//  Serial.print('|');
-//  Serial.print(baseline);
-//  Serial.print('|');
-//  Serial.print(ticks);
-//  Serial.print('|');
+  unsigned int millis_start = millis();
+  while (Wire.available() < 3) {
+    if (io_timeout > 0 && ((unsigned int)millis() - millis_start) > io_timeout) {
+      log("Failed to read Depth from I2C");
+      return;
+    }
+  }
+  ByteHigh = Wire.read();
+  ByteMiddle = Wire.read();
+  ByteLow = Wire.read();
+  
+  AdcPressure = ((long)ByteHigh << 16) + ((long)ByteMiddle << 8) + (long)ByteLow;
+  
+//  log("D1 is: ");
+//  log(AdcPressure);
+  
+  sendCommand(D2_512);
+  delay(10);
+  sendCommand(AdcRead);
+  Wire.requestFrom(DevAddress, 3);
+ 
+  millis_start = millis();
+  while (Wire.available() < 3) {
+    if (io_timeout > 0 && ((unsigned int)millis() - millis_start) > io_timeout) {
+      log("Failed to read Depth from I2C");
+      return;
+    }
+  }
+  ByteHigh = Wire.read();
+  ByteMiddle = Wire.read();
+  ByteLow = Wire.read();
 
-  if (statustime.elapsed(100)) {
-    envdata::PRES = sensor.pressure();
-    envdata::TEMP = sensor.temperature();
-  }  
+  AdcTemperature = ((long)ByteHigh << 16) + ((long)ByteMiddle << 8) + (long)ByteLow;
+ // log("D2 is: ");
+//  log(AdcTemperature);
+  
+    
+  // Calculate the Temperature (first-order computation)
+  
+  TempDifference = (float)(AdcTemperature - ((long)CalConstant[5] << 8));
+  Temperature = (TempDifference * (float)CalConstant[6])/ pow(2, 23);
+  Temperature = Temperature + 2000;  // This is the temperature in hundredths of a degree C
+  
+  // Calculate the second-order offsets
+  
+  if (Temperature < 2000.0)  // Is temperature below or above 20.00 deg C ?
+  {
+    T2 = 3 * pow(TempDifference, 2) / pow(2, 33);
+    Off2 = 1.5 * pow((Temperature - 2000.0), 2);
+    Sens2 = 0.625 * pow((Temperature - 2000.0), 2);
+  }
+  else
+  {
+    T2 = (TempDifference * TempDifference) * 7 / pow(2, 37);
+    Off2 = 0.0625 * pow((Temperature - 2000.0), 2); 
+    Sens2 = 0.0;
+  }
+  
+  // Check print the offsets
+  
+  //log("Second-order offsets are:");
+  //log(T2);
+  //log(Off2);
+  //log(Sens2);
+  
+  
+  // Print the temperature results
+  
+  Temperature = Temperature / 100;  // Convert to degrees C
+  Serial.print("First-Order Temperature in Degrees C is ");
+  Serial.println(Temperature);
+  Serial.print("Second-Order Temperature in Degrees C is ");
+  Serial.println(Temperature - (T2 / 100));
+  envdata::TEMP = (Temperature-T2) / 100;  
+  // Calculate the pressure parameters
+  
+  Offset = (float)CalConstant[2] * pow(2,16);
+  Offset = Offset + ((float)CalConstant[4] * TempDifference / pow(2, 7));
 
+  Sensitivity = (float)CalConstant[1] * pow(2, 15);
+  Sensitivity = Sensitivity + ((float)CalConstant[3] * TempDifference / pow(2, 8));
+  
+  // Add second-order corrections
+  
+  Offset = Offset - Off2;
+  Sensitivity = Sensitivity - Sens2;
+  
+  // Calculate absolute pressure in bars
+
+  Pressure = (float)AdcPressure * Sensitivity / pow(2, 21);
+  Pressure = Pressure - Offset;
+  Pressure = Pressure / pow(2, 15);
+  Pressure = Pressure / 10;  // Set output to mbars = hectopascal;
+
+  envdata::PRES = Pressure;
+
+  // Convert to psig and display
+  //
+  Pressure = Pressure - 1.015;  // Convert to gauge pressure (subtract atmospheric pressure)
+  Pressure = Pressure * 14.50377;  // Convert bars to psi
+  //log("Pressure in psi is: ");
+  //log(Pressure);
+  
+  }
 }
 
-//
-//  MS5803.cpp
-//
-//  This library is for reading and writing to the MS5803 pressure/temperature sensor.
-//
-//  Created by Victor Konshin on 4/10/13.
-//
-//
-//  Copyright (c) 2013, Victor Konshin, info@ayerware.com
-//  for the DIY Dive Computer project www.diydivecomputer.com
-//  All rights reserved.
-
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//  * Redistributions of source code must retain the above copyright
-//  notice, this list of conditions and the following disclaimer.
-//  * Redistributions in binary form must reproduce the above copyright
-//  notice, this list of conditions and the following disclaimer in the
-//  documentation and/or other materials provided with the distribution.
-//  * Neither the name of Ayerware Publishing nor the
-//  names of its contributors may be used to endorse or promote products
-//  derived from this software without specific prior written permission.
-
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-//  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-
-#include <SPI.h>
-#include <Wire.h>
-
-// Sensor constants:
-#define SENSOR_CMD_RESET      0x1E
-#define SENSOR_CMD_ADC_READ   0x00
-#define SENSOR_CMD_ADC_CONV   0x40
-#define SENSOR_CMD_ADC_D1     0x00
-#define SENSOR_CMD_ADC_D2     0x10
-#define SENSOR_CMD_ADC_256    0x00
-#define SENSOR_CMD_ADC_512    0x02
-#define SENSOR_CMD_ADC_1024   0x04
-#define SENSOR_CMD_ADC_2048   0x06
-#define SENSOR_CMD_ADC_4096   0x08
-
-#define SENSOR_I2C_ADDRESS    0x76 // If the CSB Pin (pin 3) is high, then the address is 0x76, if low, then it's 0x77
-
-static unsigned int      sensorCoefficients[8];           // calibration coefficients
-static unsigned long     D1                       = 0;    // Stores uncompensated pressure value
-static unsigned long     D2                       = 0;    // Stores uncompensated temperature value
-static float             deltaTemp                = 0;    // These three variable are used for the conversion.
-static float             sensorOffset             = 0;
-static float             sensitivity              = 0;
-static unsigned int      io_timeout               = 60;
-// Constructor when using SPI.
-MS5803::MS5803(uint8_t cs) {
-    _cs   = cs;
-    interface = true;
-}
-
-// Constructor when using i2c.
-MS5803::MS5803() {
-	interface = false;
-}
-
-boolean MS5803::initalizeSensor() {
-    
-    // Start the appropriate interface.
-    if (interface) {
-        pinMode( _cs, OUTPUT );
-  	    digitalWrite( _cs, HIGH );
-    	SPI.begin();
-   	    SPI.setBitOrder( MSBFIRST );
-   	    SPI.setClockDivider( SPI_CLOCK_DIV2 ); // Go fast or go home...
-    } else {
-        Wire.begin();
-    }
-    
-    // resetting the sensor on startup is important
-    resetSensor(); 
-	
-	// Read sensor coefficients - these will be used to convert sensor data into pressure and temp data
-    for (int i = 0; i < 8; i++ ){
-        sensorCoefficients[ i ] = ms5803ReadCoefficient( i );  // read coefficients
-        Serial.print("Coefficient = ");
-        Serial.println(sensorCoefficients[ i ]);
-        delay(10);
-    }
-    
-    unsigned char p_crc = sensorCoefficients[ 7 ];
-    unsigned char n_crc = ms5803CRC4( sensorCoefficients ); // calculate the CRC
-    
-    // If the calculated CRC does not match the returned CRC, then there is a data integrity issue.
-    // Check the connections for bad solder joints or "flakey" cables. 
-    // If this issue persists, you may have a bad sensor.
-    if ( p_crc != n_crc ) {
-        return false;
-    }
-    
-    return true;
-}
-
-void MS5803::readSensor() {
-//swapped D1 D2... (BA)
-	// If power or speed are important, you can change the ADC resolution to a lower value.
-	// Currently set to SENSOR_CMD_ADC_4096 - set to a lower defined value for lower resolution.
-	D2 = ms5803CmdAdc( SENSOR_CMD_ADC_D1 + SENSOR_CMD_ADC_4096 );    // read uncompensated pressure
-    D1 = ms5803CmdAdc( SENSOR_CMD_ADC_D2 + SENSOR_CMD_ADC_4096 );    // read uncompensated temperature
-    
-    // calculate 1st order pressure and temperature correction factors (MS5803 1st order algorithm). 
-    deltaTemp = D2 - sensorCoefficients[5] * pow( 2, 8 );
-    sensorOffset = sensorCoefficients[2] * pow( 2, 16 ) + ( deltaTemp * sensorCoefficients[4] ) / pow( 2, 7 );
-    sensitivity = sensorCoefficients[1] * pow( 2, 15 ) + ( deltaTemp * sensorCoefficients[3] ) / pow( 2, 8 );
-    
-    // calculate 2nd order pressure and temperature (MS5803 2st order algorithm)
-    temp = ( 2000 + (deltaTemp * sensorCoefficients[6] ) / pow( 2, 23 ) ) / 100; 
-    press = ( ( ( ( D1 * sensitivity ) / pow( 2, 21 ) - sensorOffset) / pow( 2, 15 ) ) / 10 );
-    
-}
-
-// Sends a power on reset command to the sensor.
-// Should be done at powerup and maybe on a periodic basis (needs to confirm with testing).
-void MS5803::resetSensor() {
-
-	if (interface) {
-    	SPI.setDataMode( SPI_MODE3 );
-   	 	digitalWrite( _cs, LOW );
-   		SPI.transfer( SENSOR_CMD_RESET );
-    	delay( 10 );
-    	digitalWrite( _cs, HIGH );
-    	delay( 5 );
-    } else {
-    	Wire.beginTransmission( SENSOR_I2C_ADDRESS );
-        Wire.write( SENSOR_CMD_RESET );
-        Wire.endTransmission();
-    	delay( 10 );
-    }
-}
-
-// These sensors have coefficient values stored in ROM that are used to convert the raw temp/pressure data into degrees and mbars.
-// This method reads the coefficient at the index value passed.  Valid values are 0-7. See datasheet for more info.
-unsigned int MS5803::ms5803ReadCoefficient(uint8_t index) {
-    unsigned int result = 0;   // result to return
-    
-    if (interface) {
-    	SPI.setDataMode( SPI_MODE3 );
-    	digitalWrite( _cs, LOW );
-    
-    	// send the device the coefficient you want to read:
-    	SPI.transfer( 0xA0 + ( index * 2 ) );
-    
-    	// send a value of 0 to read the first byte returned:
-    	result = SPI.transfer( 0x00 );
-    	result = result << 8;
-    	result |= SPI.transfer( 0x00 ); // and the second byte
-    
-    	// take the chip select high to de-select:
-    	digitalWrite( _cs, HIGH );
-    	
-    } else {
-	    Wire.beginTransmission( SENSOR_I2C_ADDRESS );
-        Wire.write( 0xA0 + ( index * 2 ) );
-        Wire.endTransmission();
-        Wire.requestFrom ( SENSOR_I2C_ADDRESS, 2 );
-          unsigned int millis_start = millis();
-          while (Wire.available() < 2) {
-            if (io_timeout > 0 && ((unsigned int)millis() - millis_start) > io_timeout) {
-    //        did_timeout = true;
-              return 0;
-            }
-          }
-        
-    	result = Wire.read();
-    	result = result << 8;
-    	result |= Wire.read(); 
-    }
-    
-    return( result );
-}
-
-// Coefficient at index 7 is a four bit CRC value for verifying the validity of the other coefficients.
-// The value returned by this method should match the coefficient at index 7.
-// If not there is something works with the sensor or the connection.
-unsigned char MS5803::ms5803CRC4(unsigned int n_prom[]) {
-
-    int cnt;
-    unsigned int n_rem;
-    unsigned int crc_read;
-    unsigned char  n_bit;
-    
-    n_rem = 0x00;
-    crc_read = sensorCoefficients[7];
-    sensorCoefficients[7] = ( 0xFF00 & ( sensorCoefficients[7] ) );
-    
-    for (cnt = 0; cnt < 16; cnt++)
-    { // choose LSB or MSB
-        if ( cnt%2 == 1 ) n_rem ^= (unsigned short) ( ( sensorCoefficients[cnt>>1] ) & 0x00FF );
-        else n_rem ^= (unsigned short) ( sensorCoefficients[cnt>>1] >> 8 );
-        for ( n_bit = 8; n_bit > 0; n_bit-- )
-        {
-            if ( n_rem & ( 0x8000 ) )
-            {
-                n_rem = ( n_rem << 1 ) ^ 0x3000;
-            }
-            else {
-                n_rem = ( n_rem << 1 );
-            }
-        }
-    }
-    
-    n_rem = ( 0x000F & ( n_rem >> 12 ) );// // final 4-bit reminder is CRC code
-    sensorCoefficients[7] = crc_read; // restore the crc_read to its original place
-    
-    return ( n_rem ^ 0x00 ); // The calculated CRC should match what the device initally returned.
-}
-
-// Use this method to send commands to the sensor.  Pretty much just used to read the pressure and temp data.
-unsigned long MS5803::ms5803CmdAdc(char cmd) {
-
-    unsigned int result = 0;
-    unsigned long returnedData = 0;
-    
-    if ( interface ) {
-    	SPI.setDataMode( SPI_MODE3 );
-    	digitalWrite( _cs, LOW );
-    	SPI.transfer( SENSOR_CMD_ADC_CONV + cmd );
-    } else {
-	    Wire.beginTransmission( SENSOR_I2C_ADDRESS );
-        Wire.write( SENSOR_CMD_ADC_CONV + cmd );
-        Wire.endTransmission();
-    }
-    
-    switch ( cmd & 0x0f )
-    {
-        case SENSOR_CMD_ADC_256 :
-            delay( 1 );
-            break;
-        case SENSOR_CMD_ADC_512 :
-            delay( 3 );
-            break;
-        case SENSOR_CMD_ADC_1024:
-            delay( 4 );
-            break;
-        case SENSOR_CMD_ADC_2048:
-            delay( 6 );
-            break;
-        case SENSOR_CMD_ADC_4096:
-            delay( 10 );
-            break;
-    }
-    
-	if ( interface ) {
-    	digitalWrite( _cs, HIGH );
-    	delay(3);
-    	digitalWrite( _cs, LOW );
-    	
-    	SPI.transfer( SENSOR_CMD_ADC_READ );
-    	
-    	returnedData = SPI.transfer( 0x00 );
-    	result = 65536 * returnedData;
-    	returnedData = SPI.transfer( 0x00 );
-    	result = result + 256 * returnedData;
-    	returnedData = SPI.transfer( 0x00 );
-    	result = result + returnedData;
-    	digitalWrite( _cs, HIGH );
-    } else {
-        delay(3);
-        Wire.beginTransmission( SENSOR_I2C_ADDRESS );
-        Wire.write( SENSOR_CMD_ADC_READ );
-        Wire.endTransmission();
-        
-        // Always read back three bytes (24 bits)
-    	Wire.requestFrom ( SENSOR_I2C_ADDRESS, 3 );
-            unsigned int millis_start = millis();
-          while (Wire.available() < 3) {
-            if (io_timeout > 0 && ((unsigned int)millis() - millis_start) > io_timeout) {
-    //        did_timeout = true;
-              return 0;
-            }
-          }
-          
-    	returnedData = Wire.read();
-    	result = 65536 * returnedData;
-    	returnedData = Wire.read();
-    	result = result + 256 * returnedData;
-    	returnedData = Wire.read();
-    	result = result + returnedData;
-    }
-    return result;
-}
 
 #endif
 
