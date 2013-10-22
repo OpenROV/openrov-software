726424c12c841f475abbac48f330f96adfc96e0e
BrianAdams <brian@Babs.local>
2013-10-21 23:43:26 -0700

    Report the I2C addresses on the Arduino at startup

--- arduino/OpenROV/Settings.cpp
+++ arduino/OpenROV/Settings.cpp
@@ -20,6 +20,7 @@ void Settings::device_loop(Command command){
       Serial.print(String(Settings::deadZone_min) + ",");
       Serial.print(F("deadZone_max|"));
       Serial.println(String(Settings::deadZone_max) + ";");
+      scan_i2c();
     }
     else if (command.cmp("rcap")){ //report capabilities
       Serial.print(F("CAPA:"));
@@ -34,6 +35,48 @@ void Settings::device_loop(Command command){
 
 }
 
+void Settings::scan_i2c(){
+  byte error, address;
+  int nDevices;
+
+  Serial.println(F"log:Scanning...;");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print(F"log:I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !;");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print(F"log:Unknow error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println(";");
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println(F"log:No I2C devices found\n;");
+  else
+    Serial.println(F"log:done\n;");
+
+  delay(5000);           // wait 5 seconds for next scan
+    
+}
 
 
 
