0f0d8e50524224cc88de1b17f9d6f2c9bc62695b
BrianAdams <brian@babastik.com>
2013-10-30 09:16:36 +0000

    Blackbox now recording with the r key

--- src/static/js/blackbox.js
+++ src/static/js/blackbox.js
@@ -4,18 +4,22 @@
         /*
          * Constructuor
          */
+	var telemetry = [];
         var Blackbox = function Blackbox(cockpit) {
                 console.log("Loading Blackbox plugin.");
                 this.cockpit = cockpit;
                 this.recording = false;
+		var blackbox = this;
                 this.cockpit.socket.on('navdata', function(data) {
-                    if (!recording) {
-                        return;
-                    }
                     if (!jQuery.isEmptyObject(data)) {                      
-                            Blackbox.logNavData(data);
+                            blackbox.logNavData(data);
                     }        
-                });                
+                });
+		this.cockpit.socket.on('status', function(data){
+                    if (!jQuery.isEmptyObject(data)) {
+                            blackbox.logStatusData(data);
+                    }
+                });
 
                 // Register the various event handlers
                 this.listen();
@@ -31,6 +35,26 @@
                 });
         };
 
+	var refreshintervalID;
+	var server;
+//	var telemetry = [];
+
+	Blackbox.prototype.toggleRecording = function toggleRecording(){
+		console.log("Recording = " + this.recording);
+		if (!this.recording){
+			console.log("Recording Telemetry");
+			var blackbox = this;
+			refreshintervalID = self.setInterval(blackbox.logTelemetryData,1000);	
+		} else {
+			console.log("Stopping Telemetry");
+			clearInterval(refreshintervalID);
+		}
+		this.recording = !this.recording;
+
+	};
+
+	Blackbox.prototype.test = function(){console.log("tteesstt");blackbox.logTelemetryData()};
+
         /*
          * Process onkeydown. 
          */
@@ -39,44 +63,74 @@
                         return;
                 } 
                 ev.preventDefault();
-
+		
                 if (!this.recording) {
-                        openDB(function (x) {this.recording = !this.recording;})
+                        this.openDB(this.toggleRecording());
                 } else {
-                        closeDB(function (x) {this.recording = !this.recording;})
+                        this.closeDB(this.toggleRecording());
                 }
         };
         
         Blackbox.prototype.logNavData = function logNavData(navdata){
+                 if (!this.recording) {
+                        return;
+                 }
                 server.navdata.add(navdata).done( function ( item ) {
                     console.log("saved");
                 } ); 
                 
         };
-        
-        var server;      
-        Blackbox.prototype.openDB = function openDB(callback){
 
+        Blackbox.prototype.logTelemetryData = function logTelemetryData(){
+		var clone = [];
+		for (i in telemetry){
+			clone[i] = telemetry[i];
+		}
+                server.telemetry.add(clone).done( function ( item ) {
+                    console.log("saved telemetry");
+                } )
+		.fail( function (a,x) {console.log(x);});
+
+        };
+
+        Blackbox.prototype.logStatusData = function logStatusData(data){
+                 if (!this.recording) {
+                        return;
+                 }
+		for (i in data){
+		  telemetry[i] = data[i];
+		}
+        };
+
+
+        
+        Blackbox.prototype.openDB = function openDB(callback){
         db.open( {
             server: 'openrov-blackbox',
-            version: 1,
+            version: 4,
             schema: {
                 navdata: {
                     key: { keyPath: 'id' , autoIncrement: true },
                     // Optionally add indexes
                     indexes: {
-                        timestamp: { },
                     }
+                },
+                telemetry: {
+                    key: { keyPath: 'id' , autoIncrement: true }
+
+                    // Optionally add indexes
                 }
             }
         } ).done( function ( s ) {
-            server = s
+	    server = s;
+            callback;
         } );
                 
         };
         
         Blackbox.prototype.closeDB = function closeDB(callback){
                 server.close();
+		callback;
         };
 
         window.Cockpit.plugins.push(Blackbox);
