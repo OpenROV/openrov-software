b28956f116802551edbb79c391ebdd042ad3b7bb
BrianAdams <brian@babastik.com>
2013-11-09 01:29:02 +0000

    Isolated the reset functionality for upload to fix uart uploads in beta

--- linux/orovconfig.sh
+++ linux/orovconfig.sh
@@ -11,14 +11,16 @@ then
     export UPLOAD_TO_ATMEGA_COMMAND="avrdude -P /dev/spidev1.0 -c linuxspi -vvv -p m2560 -U flash:w:.build/mega2560/firmware.hex"
     export BUILD_ATMEGA_CODE="ino build -m mega2560"
     export LINUX_RESET_GPIO=30
+    export UPLOAD_REQUIRES_RESET=false
 fi
 
 
 if test "$ROV_BOARD" = "cape"
 then
-    export UPLOAD_TO_ATMEGA_COMMAND="(sleep 0.0$COUNTER && /opt/openrov/linux/reset.sh 1>&2); avrdude -P /dev/ttyO1 -c arduino -D -vvv -p m328p -U flash:w:.build/uno/firmware.hex"
+    export UPLOAD_TO_ATMEGA_COMMAND="avrdude -P /dev/ttyO1 -c arduino -D -vvv -p m328p -U flash:w:.build/uno/firmware.hex"
     export BUILD_ATMEGA_CODE="ino build"
     export LINUX_RESET_GPIO=32
+    export UPLOAD_REQUIRES_RESET=true
 fi
 
 
