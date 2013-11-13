7715f57623b21c113a77d9edcc5c0f1d37c389cc
BrianAdams <brian@babastik.com>
2013-11-13 02:02:02 +0000

    Telemetry plugin working

--- src/plugins/telemetry/public/js/telemetry.js
+++ src/plugins/telemetry/public/js/telemetry.js
@@ -8,38 +8,55 @@
 
         // Instance variables
         this.cockpit = cockpit;
+	this.telemetry = new Object();
+	this.rawTelemetry = ko.observableArray([]);
 
         // Add required UI elements
-	$("#notfound").after(
-                '<div id="telemetry" class="controller well well-small" > \
-\
+	$("#rov_status_panel").append(
+                '<div id="telemetry" class="controller well well-small" >\
 		    <ul data-bind="foreach: rawTelemetry">\
 		       <li>\
 		           <span data-bind="text: key"></span>\
 		           <span data-bind="text: value"></span>\
 		       </li>\
 		    </ul>\
-\
 		</div>');
 	
+	var self = this;
+        setInterval(function(){self.displayTelemetry();}, 1000);	
         // Register the various event handlers
         this.listen();
+	ko.applyBindings(this);
         
     };
     
     //This pattern will hook events in the cockpit and pull them all back
     //so that the reference to this instance is available for further processing
-    Motor_diags.prototype.listen = function listen() {
+    Telemetry.prototype.listen = function listen() {
         var self = this;
-        $("#diagnostic .back-button").click(function (){
-            self.SaveSettings();
-        });
-        this.cockpit.socket.on('settings', function(data) {
-            self.LoadSettings(data);
+
+	this.cockpit.socket.on('status', function(data) {
+            self.logStatusData(data);
         });
 
     };
+    
+    Telemetry.prototype.logStatusData = function logStatusData(data){
+	for (var i in data){
+	  this.telemetry[i] = data[i];
+	};
+   };
 
+    Telemetry.prototype.displayTelemetry = function displayTelemetry(){
+	this.rawTelemetry([]);
+	for (var item in this.telemetry){
+	  if (this.telemetry.hasOwnProperty(item)) {
+	    this.rawTelemetry().push({ key: item, value: this.telemetry[item] });
+	  }
+	};
+		
+	this.rawTelemetry.valueHasMutated();
+   };
 
     window.Cockpit.plugins.push(Telemetry);
 
