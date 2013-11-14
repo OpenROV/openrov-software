0ddae823b524f8c8af3c590be7990b2bcc3a3a16
BrianAdams <brian@babastik.com>
2013-11-14 05:30:42 +0000

    Refactored keyboard control and tested

--- arduino/OpenROV/Lights.cpp
+++ arduino/OpenROV/Lights.cpp
@@ -14,18 +14,20 @@ void Lights::device_setup(){
   light.write(0);
 }
 
+
+
 void Lights::device_loop(Command command){
 
-    if( command.cmp("light")){
-      int value = command.args[1];
+    if( command.cmp("ligt")){
+      int value = command.args[1]; //0 - 255
       light.write(value);
       
       Serial.print(F("LIGT:"));
       Serial.print(value);
       Serial.print(';');
       Serial.print(F("LIGP:"));
-      Serial.print(value/255);
-      Serial.print(';');       
+      Serial.print(command.args[1]/255.0);
+      Serial.println(';');       
     }  
 }
 #endif
