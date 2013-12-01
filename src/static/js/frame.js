740b555ef3f64c7a6895f552a764aa874edbf523
BrianAdams <brian@babastik.com>
2013-12-01 05:55:47 +0000

    Removed video controls

--- src/static/js/frame.js
+++ src/static/js/frame.js
@@ -4,20 +4,9 @@ function setupFrameHandling(socket) {
 		var address = 'http://' + socket.socket.options.host + ':' + CONFIG.video_port + '/?action=stream';
 	   	$('#video').attr('poster', address);
 		console.log('video enabled');
-		
-	/*videoChecker = self.setInterval(function(){
-		if($('#video').width()<100){
-			//Houston we have a problem
-			$('#video').attr('src', "");
-			$('#video').attr('src', address);
-			console.log('video restarted');
-			}
-		},1000);*/	
 	});
 	
 	socket.on('VideoStopped', function(data) {
-		clearInterval(videoChecker);
 		console.log('video stopped');
-		$('#video').attr('src', 'themes/OpenROV/img/no_camera.jpg');
 	});
 }
