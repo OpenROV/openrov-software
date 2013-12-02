7b5aef9bfd62ca8aa01c58b99b8acf638d1af31a
codewithpassion <dominik.fretz@gmail.com>
2013-12-03 10:56:46 +1100

    Make start/stop work from dashboard

--- dashboard/src/static/js/DashboardViewModel.js
+++ dashboard/src/static/js/DashboardViewModel.js
@@ -1,15 +1,24 @@
 function DashboardViewModel(socket){
-    var self = this;
+    var viewModel = {
+        cockpitStatus : ko.observable(new ProcessModel()),
+        cloud9Status : ko.observable(new ProcessModel()),
+        networkshareStatus : ko.observable(new ProcessModel()),
 
-    self.cockpitStatus = ko.observable(new ProcessModel());
-    self.cloud9Status = ko.observable(new ProcessModel());
-    self.networkshareStatus = ko.observable(new ProcessModel());
+        cockpitStart : function() {
+            socket.emit('start-cockpit');
+        },
+
+        cockpitStop : function() {
+            socket.emit('stop-cockpit');
+        }
+    };
 
     socket.on('status-cockpit', function(status){
-    	self.cockpitStatus().status(status);
-    	self.cockpitStatus().running((status === "Running"));
+        viewModel.cockpitStatus().status(status);
+        viewModel.cockpitStatus().running((status === "Running"));
     });
-    return self;
+
+    return viewModel;
 }
 
 function ProcessModel() {
