dfba9a61c8b0a825134a839457ec075e08d9ca91
codewithpassion <dominik.fretz@gmail.com>
2013-12-04 21:41:57 +1100

    Introducing layer inbetween mock and app

--- dashboard/src/plugins/01_cockpit/index.js
+++ dashboard/src/plugins/01_cockpit/index.js
@@ -3,15 +3,15 @@ function cockpit(name, deps) {
 	deps.io.sockets.on('connection', function (socket) {
 
 		socket.on('status-cockpit', function(){
-			deps.dashboardEngine.send({ key: 'status-cockpit' });
+			deps.dashboardEngine.emit('signal', { key: 'status-cockpit' });
 		});
 
 		socket.on('start-cockpit', function() {
-			deps.dashboardEngine.send({ key: 'start-cockpit' });
+			deps.dashboardEngine.emit('signal', { key: 'start-cockpit' });
 		});
 
 		socket.on('stop-cockpit', function() {
-			deps.dashboardEngine.send({ key: 'stop-cockpit' });
+			deps.dashboardEngine.emit('signal', { key: 'stop-cockpit' });
 		});
 
 	});
