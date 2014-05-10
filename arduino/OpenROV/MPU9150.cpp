0a9bf7ecff7b39cfd3e6477976e5f14069248449
BrianAdams <brian@babastik.com>
2014-05-09 17:51:56 -1000

    Added the tuning parameters for the MPU9150 library

--- arduino/OpenROV/MPU9150.cpp
+++ arduino/OpenROV/MPU9150.cpp
@@ -19,13 +19,33 @@ CALLIB_DATA calData;
 Timer calibration_timer;
 //  MPU_UPDATE_RATE defines the rate (in Hz) at which the MPU updates the sensor data and DMP output
 
-#define MPU_UPDATE_RATE  (10)
+//  MPU_UPDATE_RATE defines the rate (in Hz) at which the MPU updates the sensor data and DMP output
+
+#define MPU_UPDATE_RATE  (20)
+
+//  MAG_UPDATE_RATE defines the rate (in Hz) at which the MPU updates the magnetometer data
+//  MAG_UPDATE_RATE should be less than or equal to the MPU_UPDATE_RATE
+
+#define MAG_UPDATE_RATE  (10)
+
+//  MPU_MAG_MIX defines the influence that the magnetometer has on the yaw output.
+//  The magnetometer itself is quite noisy so some mixing with the gyro yaw can help
+//  significantly. Some example values are defined below:
+
+#define  MPU_MAG_MIX_GYRO_ONLY          0                   // just use gyro yaw
+#define  MPU_MAG_MIX_MAG_ONLY           1                   // just use magnetometer and no gyro yaw
+#define  MPU_MAG_MIX_GYRO_AND_MAG       10                  // a good mix value
+#define  MPU_MAG_MIX_GYRO_AND_SOME_MAG  50                  // mainly gyros with a bit of mag correction
+
+//  MPU_LPF_RATE is the low pas filter rate and can be between 5 and 188Hz
+
+#define MPU_LPF_RATE   40
 
 void MPU9150::device_setup(){
   //Todo: Read calibration values from EPROM
   Wire.begin();
   MPU.selectDevice(1);
-  MPU.init(MPU_UPDATE_RATE);                             // start the MPU
+  MPU.init(MPU_UPDATE_RATE, MPU_MAG_MIX_GYRO_AND_MAG, MAG_UPDATE_RATE, MPU_LPF_RATE);
   Settings::capability_bitarray |= (1 << COMPASS_CAPABLE);
   Settings::capability_bitarray |= (1 << ORIENTATION_CAPABLE);
 }
