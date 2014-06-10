1a0377cee1e14743c06a03d4b80458ce514a6226
root <root@OpenROV.(none)>
2014-06-10 05:07:54 +0000

    Adding ready plugin
    Signed-off-by: codewithpassion <dominik.fretz@gmail.com>
    
    Fixing ready plugin to flash the rov lights when we start

---
+++ src/plugins/ready/index.js
@@ -0,0 +1,54 @@
+function readyAyeReady(name, deps) {
+  var done = false;
+  var lastLightCmd = "";
+
+  //rovsys comes up when arduino starts
+  deps.rov.on('rovsys', function(s){
+    if (! done) {
+      console.log('The ROV is ready!.');
+      setLight('12.5');
+      done = true;
+    }
+  });
+
+  // send the light command initially and then cylcle through the lights
+  var setLight = function(light) {
+      var cmd = 'ligt(' + light + ')';
+      deps.rov.send(cmd);
+      var chk = setInterval(function() {
+       if (lastLightCmd != cmd) {
+         deps.rov.send(cmd);
+       }
+       else {
+         clearInterval(chk);
+         setTimeout(function() { deps.rov.send('ligt(24.5)')}, 500);
+         setTimeout(function() { deps.rov.send('ligt(40.5)')}, 1000);
+         setTimeout(function() { deps.rov.send('ligt(24.5)')}, 1500);
+         setTimeout(function() { deps.rov.send('ligt(12.5)')}, 2000);
+         setTimeout(function() { deps.rov.send('ligt(0)')}, 2500);
+         setTimeout(function() { deps.rov.send('ligt(50)')}, 3500);
+         setTimeout(function() { deps.rov.send('ligt(0)')}, 4000);
+         setTimeout(function() { deps.rov.send('ligt(50)')}, 4500);
+         setTimeout(function() { deps.rov.send('ligt(0)')}, 5000);
+        }}, 400);
+  }
+
+
+  // to get the Arduino ready, request the capabilities
+  var checkId = setInterval(
+     function() {
+      if (! deps.rov.notSafeToControl()) {
+        clearInterval(checkId);
+      }
+      else {
+        deps.rov.requestCapabilities();
+      }
+    },
+    200);
+
+  deps.rov.on('command', function(command) {
+   lastLightCmd = command;
+  });
+
+}
+module.exports = readyAyeReady ;
