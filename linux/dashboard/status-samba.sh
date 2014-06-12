6fa5eef566cd6b6540d66bfde558a9946b4b27e8
codewithpassion <dominik.fretz@gmail.com>
2014-06-11 19:18:24 -0700

    Fixing samba status for debian

--- linux/dashboard/status-samba.sh
+++ linux/dashboard/status-samba.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-STATUS=`status smbd`
-if [ "$STATUS" = "smbd stop/waiting" ]; then #stopped
-	exit 1
+service samba status
+export RESULT=$?
+if [ $RESULT = 0 ]; then #running
+        exit 0
+else
+        exit 1
 fi
-exit 0
+
+
