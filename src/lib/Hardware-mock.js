12194c8b254204f9061da7dffba6ddf02d881b91
codewithpassion <dominik.fretz@gmail.com>
2014-06-18 10:47:23 -0700

    Implementing UI elements for depth and heading hold

--- src/lib/Hardware-mock.js
+++ src/lib/Hardware-mock.js
@@ -2,6 +2,9 @@ var EventEmitter = require('events').EventEmitter, StatusReader = require('./Sta
 function Hardware() {
   var hardware = new EventEmitter();
   var reader = new StatusReader();
+  hardware.depthHoldEnabled = false;
+  hardware.targetHoldEnabled = false;
+
   reader.on('Arduino-settings-reported', function (settings) {
     hardware.emit('Arduino-settings-reported', settings);
   });
@@ -23,6 +26,38 @@ function Hardware() {
       hardware.emit('status', reader.parseStatus('servo:' + commandParts[1]));
       console.log('HARDWARE-MOCK return servo status');
     }
+
+    // Depth hold
+    if (commandText === 'holdDepth_toggle') {
+        var targetDepth = 0;
+        if (!hardware.depthHoldEnabled) {
+            targetDepth = 10;
+            hardware.depthHoldEnabled = true;
+            console.log('HARDWARE-MOCK depth hold enabled');
+        }
+        else {
+            targetDepth = -500;
+            hardware.depthHoldEnabled = false
+            console.log('HARDWARE-MOCK depth hold DISABLED');
+        }
+        hardware.emit('status', reader.parseStatus('targetDepth:' + targetDepth));
+    }
+
+    // Heading hold
+    if (commandText === 'holdHeading_toggle') {
+        var targetHeading = 0;
+        if (!hardware.targetHoldEnabled) {
+            targetHeading = 10;
+            hardware.targetHoldEnabled= true;
+            console.log('HARDWARE-MOCK heading hold enabled');
+        }
+        else {
+            targetHeading = -500;
+            hardware.targetHoldEnabled = false
+            console.log('HARDWARE-MOCK heading hold DISABLED');
+        }
+        hardware.emit('status', reader.parseStatus('targetHeading:' + targetHeading));
+    }
   };
   hardware.close = function () {
     console.log('!Serial port closed');
@@ -34,7 +69,7 @@ function Hardware() {
   }, 1000);
   setInterval(sendEvent, 3000);
   function sendEvent() {
-    var data = 'vout:1023;iout:125;';
+    var data = 'vout:8.3;iout:0.2;';
     var status = reader.parseStatus(data);
     hardware.emit('status', status);
   }
