d9f73a0decc23c325ad1091cc126980916fab3e3
codewithpassion <dominik.fretz@gmail.com>
2014-06-20 11:56:25 -0700

    Improving the disabled state for Depth and heading hold and adding an image to the laser indicator

--- arduino/OpenROV/Pilot.cpp
+++ arduino/OpenROV/Pilot.cpp
@@ -62,7 +62,7 @@ void Pilot::device_loop(Command command){
         Serial.println(';');
       }
       Serial.print(F("targetHeading:"));
-      Serial.print(tgt_Hdg);
+      Serial.print(_headingHoldEnabled ? tgt_Hdg : DISABLED);
       Serial.println(';');
     }
 
@@ -71,7 +71,7 @@ void Pilot::device_loop(Command command){
       if (_depthHoldEnabled) {
         _depthHoldEnabled = false;
         raw_lift = 0;
-        target_depth = -500;  // -500 = system not in hdg hold
+        target_depth = 0;
 
         int argsToSend[] = {1,0}; //include number of parms as last parm
         command.pushCommand("lift",argsToSend);
@@ -90,7 +90,7 @@ void Pilot::device_loop(Command command){
         Serial.println(';');
       }
       Serial.print(F("targetDepth:"));
-      Serial.print(target_depth);
+      Serial.print(_depthHoldEnabled ? target_depth : DISABLED);
       Serial.println(';');
     }
 
