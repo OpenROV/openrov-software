958f35ff7bf6281c2aba9222168afbeaf1efbc61
codewithpassion <dominik.fretz@gmail.com>
2014-06-25 04:14:24 -0700

    Initial plugin manager code. binding doesn't work right now

--- src/plugins/plugin-manager/public/js/index.js
+++ src/plugins/plugin-manager/public/js/index.js
@@ -1,12 +1,27 @@
 (function (window, $, undefined) {
-    'use strict';
-    var PluginManager;
-    PluginManager = function Example(cockpit) {
-        console.log('Loading Plugin Manager plugin.');
-        this.cockpit = cockpit;
+  'use strict';
 
-        $('#settings #plugin-settings').append('<div id="plugin-manager-settings"></div>');
-        $('#plugin-manager-settings').load('plugin/plugin-manager/settings.html', function () {});
-    };
-    window.Cockpit.plugins.push(PluginManager);
+  var PluginManagerModel = function PluginManagerModel() {
+    var self = this;
+    self.controlablePlugins = ko.observableArray();
+  }
+
+  var PluginManager;
+  PluginManager = function PluginManager(cockpit) {
+    console.log('Loading Plugin Manager plugin.');
+    this.cockpit = cockpit;
+    this.model = new PluginManagerModel();
+    var self = this;
+
+    $('#settings #plugin-settings').append('<div id="plugin-manager-settings"></div>');
+    $('#plugin-manager-settings').load('plugin/plugin-manager/settings.html',
+      function() {
+        cockpit.loadedPlugins.forEach(function (plugin) {
+          if (plugin.canBeDisabled) { self.model.controlablePlugins.push(plugin); }
+        });
+        ko.applyBindings(this.model);
+
+      });
+  };
+  window.Cockpit.plugins.push(PluginManager);
 }(window, jQuery));
