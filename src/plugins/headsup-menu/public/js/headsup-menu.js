3fe9aff752bb9993d654ad8d7f25503c5a1580e4
codewithpassion <dominik.fretz@gmail.com>
2014-07-23 15:28:10 +0200

    Remove debug messages

--- src/plugins/headsup-menu/public/js/headsup-menu.js
+++ src/plugins/headsup-menu/public/js/headsup-menu.js
@@ -85,7 +85,6 @@
 
     var leftHook = function() {
       var currentId = $(menuItems[currentSelected]).attr('id');
-      console.log("####left hook");
       self.items()
         .forEach(function(item) {
           if (item.uniqueId == currentId) {
@@ -98,7 +97,6 @@
 
     var rightHook = function() {
       var currentId = $(menuItems[currentSelected]).attr('id');
-      console.log("####right hook");
       self.items()
         .forEach(function(item) {
           if (item.uniqueId == currentId) {
