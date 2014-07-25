80f3109b31f3f51363cc1544432dccd232379128
codewithpassion <dominik.fretz@gmail.com>
2014-07-25 12:03:10 +0200

    Cleanup layout and make fadeIn/Out configurable

--- src/static/js/knockout-extentions.js
+++ src/static/js/knockout-extentions.js
@@ -18,3 +18,37 @@ ko.validation.rules.isUnique = {
   },
   message: 'This value is a duplicate'
 };
+
+ko.bindingHandlers.fadeVisible = {
+  init: function(element, valueAccessor) {
+    // Initially set the element to be instantly visible/hidden depending on the value
+    var value = valueAccessor();
+    var itemValue = value;
+    if (typeof value == "object") { // passing in parameters
+      itemValue = value.item;
+    }
+    $(element).toggle(ko.utils.unwrapObservable(itemValue)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+  },
+  update: function(element, valueAccessor) {
+    // Whenever the value subsequently changes, slowly fade the element in or out
+    var value = valueAccessor();
+    var itemValue = value;
+    var fadeIn = true;
+    var fadeOut = true;
+    if (typeof value == "object") { // passing in parameters
+      itemValue = value.value;
+      if ('fadeIn' in value) {
+        fadeIn = value.fadeIn;
+      }
+      if ('fadeOut' in value) {
+        fadeOut = value.fadeOut;
+      }
+    }
+    if (ko.utils.unwrapObservable(itemValue)) {
+      fadeIn ? $(element).fadeIn() : $(element).show();
+    }
+    else {
+      fadeOut ? $(element).fadeOut() : $(element).hide();
+    }
+  }
+};
