40d0d1cbcafb5021a8f5bb508262ec3e7e19ba2a
codewithpassion <dominik.fretz@gmail.com>
2014-07-25 11:36:04 +0200

    Cleaup UI indicators

--- src/plugins/aux-servo/public/js/aux-servo.js
+++ src/plugins/aux-servo/public/js/aux-servo.js
@@ -148,5 +148,17 @@
 
   };
 
+  ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+      // Initially set the element to be instantly visible/hidden depending on the value
+      var value = valueAccessor();
+      $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function(element, valueAccessor) {
+      // Whenever the value subsequently changes, slowly fade the element in or out
+      var value = valueAccessor();
+      ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+    }
+  };
   window.Cockpit.plugins.push(AuxServo);
 }(window, jQuery));
