198dec20878033e27a551531676325f01a339f4a
BrianAdams <brian@babastik.com>
2014-09-11 19:38:49 -0700

    fixed problem in auto loading afro esc firmware

--- linux/install-afro-esc-firmware.sh
+++ linux/install-afro-esc-firmware.sh
@@ -9,10 +9,10 @@ fi
 /etc/init.d/openrov stop
 arduino/firmware-install-usblinker.sh
 avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:afro_firmware/afro_nfet.hex:i -vv
-stty -F /dev/ttyO1 19200
-echo "\$M<P52" > /dev/ttyO1
+stty -F /dev/ttyO1 raw 19200
+echo -n "\$M<P52" > /dev/ttyO1
 avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:afro_firmware/afro_nfet.hex:i -vv
-echo "\$M<P53" > /dev/ttyO1
+echo -n "\$M<P53" > /dev/ttyO1
 avrdude -p m8 -b 19200 -P /dev/ttyO1 -c avrispv2 -e -U flash:w:afro_firmware/afro_nfet.hex:i -vv
 arduino/firmware-installfromsource.sh
 /etc/init.d/openrov restart
