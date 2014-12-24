f7b29a9dbccd7478220d5f8e82232ad62fcb0ece
BrianAdams <brian@babastik.com>
2014-12-24 02:06:33 -0800

    apt-update

--- payload.sh
+++ payload.sh
@@ -5,6 +5,7 @@ echo 'nameserver 208.67.222.222' > /etc/resolv.conf
 echo 'nameserver 208.67.220.220' >> /etc/resolv.conf
 
 # install node.js
+apt-get update -qq
 apt-get install curl
 curl -sL https://deb.nodesource.com/setup | bash -
 apt-get install -y nodejs
