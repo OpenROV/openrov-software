a404511f015bc51a2e942c8f6a6535f0405bb3b8
BrianAdams <brian@babastik.com>
2014-12-24 01:59:41 -0800

    install npm

--- payload.sh
+++ payload.sh
@@ -4,6 +4,11 @@ set -e
 echo 'nameserver 208.67.222.222' > /etc/resolv.conf
 echo 'nameserver 208.67.220.220' >> /etc/resolv.conf
 
+# install node.js
+apt-get install curl
+curl -sL https://deb.nodesource.com/setup | bash -
+apt-get install -y nodejs
+
 npm install --arch=armhf
 
 cd src/static
