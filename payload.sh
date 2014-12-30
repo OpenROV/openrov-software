63b987e417f90eb7024d0d3eaeec6939ae4d0e2f
BrianAdams <brian@babastik.com>
2014-12-30 14:33:53 -0800

    add ifconfig output

--- payload.sh
+++ payload.sh
@@ -2,6 +2,7 @@
 set -e
 
 cat /etc/hosts
+ifconfig
 echo 'nameserver 8.8.4.4' > /etc/resolv.conf
 echo 'nameserver 208.67.222.222' >> /etc/resolv.conf
 echo 'nameserver 208.67.220.220' >> /etc/resolv.conf
