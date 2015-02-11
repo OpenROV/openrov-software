ff2b43c8a253ffdb615ee22d33a705969718c04e
codewithpassion <dominik.fretz@gmail.com>
2015-02-11 22:27:07 +1100

    moving ping

--- src/plugins/fpscounter/public/js/fpscounter.js
+++ src/plugins/fpscounter/public/js/fpscounter.js
@@ -32,52 +32,15 @@
         history: 20
       });
       self.meter.hide();
-      $('#footercontent').append('<span class=\'pull-left\' id=\'fpsmeter2\'></span>');
-      self.beagletoBrowserMeter = new FPSMeter($('#fpsmeter2')[0], {
-        interval: 1000,
-        smoothing: 20,
-        show: 'ms',
-        toggleOn: 'click',
-        decimals: 1,
-        maxFps: 2,
-        threshold: 1000,
-        position: 'absolute',
-        zIndex: 10,
-        left: '0px',
-        top: '30px',
-        right: 'auto',
-        bottom: 'auto',
-        margin: '0 0 0 0',
-        theme: 'colorful',
-        heat: 1,
-        graph: 1,
-        history: 20
-      });
-      /*
-            setInterval(function() {
-                var bmeter = beagletoBrowserMeter;
-                bmeter.tickStart();
-                var _starttime = new Date();
-                FPSCounter.ping('192.168.1.27',function(target,port,status){
-                    bmeter.tick();
-                    var _endtime = new Date();
-                }, 8080, 300)
-            },500);
-            */
+
+
       var _self = self;
       var mysocket = _socket;
       setInterval(function () {
         _self.meter.tick();
       }, 32);
-      //This is now also used for the deadman switch.
-      setInterval(function () {
-        var _starttime = new Date();
-        _self.beagletoBrowserMeter.tickStart();
-        mysocket.emit('ping', _starttime);
-      }, 500);
-      _socket.on('pong', function (id) {
-        _self.beagletoBrowserMeter.tick();
-      });
+
+
     });
   };
   FPSCounter.prototype.listen = function listen() {
