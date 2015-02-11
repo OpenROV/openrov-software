5a1b1610a2f240365545419fd9a9c160c54db07f
codewithpassion <dominik.fretz@gmail.com>
2015-02-11 18:05:39 +1100

    Cleanup

--- src/plugins/googletalk_ipregistration/public/js/googletalk_ipregistration.js
+++ src/plugins/googletalk_ipregistration/public/js/googletalk_ipregistration.js
@@ -1,6 +1,7 @@
 (function (window, $, undefined) {
   'use strict';
   var Googletalk_ipregistraion;
+  var settingsElement;
   Googletalk_ipregistraion = function Googletalk_ipregistraion(cockpit) {
     console.log('Loading Googletalk_ipregistration plugin in the browser.');
     // Instance variables
@@ -12,7 +13,8 @@
 
     this.cockpit.extensionPoints.settingsElement
       .append('<div id="gtalk-settings"></div>');
-    $('#gtalk-settings').load(jsFileLocation + '../settings.html');
+    settingsElement = this.cockpit.extensionPoints.settingsElement.find('#gtalk-settings');
+    settingsElement.load(jsFileLocation + '../settings.html');
   };
   //This pattern will hook events in the cockpit and pull them all back
   //so that the reference to this instance is available for further processing
@@ -27,16 +29,16 @@
   };
   Googletalk_ipregistraion.prototype.LoadSettings = function LoadSettings(config) {
     if ('googletalk_rovid' in config)
-      $('#googleTalkROVid').val(config.googletalk_rovid);
+      settingsElement.find('#googleTalkROVid').val(config.googletalk_rovid);
     if ('googletalk_rovpassword' in config)
-      $('#googleTalkROVpassword').val(config.googletalk_rovpassword);
+      settingsElement.find('#googleTalkROVpassword').val(config.googletalk_rovpassword);
     if ('googletalk_rov_pilotid' in config)
-      $('#googleTalkPilotId').val(config.googletalk_rov_pilotid);
+      settingsElement.find('#googleTalkPilotId').val(config.googletalk_rov_pilotid);
   };
   Googletalk_ipregistraion.prototype.SaveSettings = function SaveSettings() {
-    this.cockpit.socket.emit('update_settings', { googletalk_rovid: $('#googleTalkROVid').val() });
-    this.cockpit.socket.emit('update_settings', { googletalk_rovpassword: $('#googleTalkROVpassword').val() });
-    this.cockpit.socket.emit('update_settings', { googletalk_rov_pilotid: $('#googleTalkPilotId').val() });
+    this.cockpit.socket.emit('update_settings', { googletalk_rovid: settingsElement.find('#googleTalkROVid').val() });
+    this.cockpit.socket.emit('update_settings', { googletalk_rovpassword: settingsElement.find('#googleTalkROVpassword').val() });
+    this.cockpit.socket.emit('update_settings', { googletalk_rov_pilotid: settingsElement.find('#googleTalkPilotId').val() });
   };
   window.Cockpit.plugins.push(Googletalk_ipregistraion);
 }(window, jQuery));
