323a055281daed0e359c83089c40228090d7dd5b
codewithpassion <dominik.fretz@gmail.com>
2015-02-18 13:10:26 +1100

    Move photoscapture

--- src/plugins/photocapture/public/js/photocapture.js
+++ src/plugins/photocapture/public/js/photocapture.js
@@ -7,16 +7,17 @@
     this.cockpit = cockpit;
     this.snapshots = ko.observableArray([]);
     // Add required UI elements
-    $('#diagnostic').after('<div class="drop-in-right" id="photos"></div>');
-    $('#menuitems').append('<li><a href="#" id="show-photos">Photos</a></li>');
-    $('#buttonPanel').append('<button id="capture-photo" class="btn">Capture</button>');
-    $('#keyboardInstructions').append('<p>press <i>c</i> to capture an image</p>');
+    $('body').append('<div id="photos"></div>');
+    cockpit.extensionPoints.menu.append('<li><a href="#" id="show-photos">Photos</a></li>');
+    cockpit.extensionPoints.buttonPanel.append('<button id="capture-photo" class="btn">Capture</button>');
+    cockpit.extensionPoints.keyboardInstructions.append('<p>press <i>c</i> to capture an image</p>');
     var self = this;
     var jsFileLocation = urlOfJsFile('photocapture.js');
     // the js folder path
-    $('#photos').load(jsFileLocation + '../photospanel.html', function () {
+    this.photos = $('body').parent().find('#photos');
+    this.photos.load(jsFileLocation + '../photospanel.html', function () {
       // Register the various event handlers
-      ko.applyBindings(self, $('#photos')[0]);
+      ko.applyBindings(self, self.photos[0]);
     });
   };
   //This pattern will hook events in the cockpit and pull them all back
@@ -32,7 +33,7 @@
       photoc.snapshots().push(filename);
       photoc.snapshots.valueHasMutated();
     });
-    $('#capture-photo').click(function () {
+    cockpit.extensionPoints.buttonPanel.find('#capture-photo').click(function () {
       photoc.cockpit.socket.emit('snapshot');
       console.log('send snapshot request to server');
     });
@@ -45,22 +46,11 @@
         down: function() { photoc.cockpit.socket.emit('snapshot'); }
       });
 
-    $('#show-photos').click(function () {
-      $('#photos').show('fold');
+    cockpit.extensionPoints.menu.find('#show-photos').click(function () {
+      photoc.photos.find('drop-in-dialog')[0].open();
       photoc.cockpit.sendUpdateEnabled = false;
-      var self = photoc;
-      Mousetrap.bind('esc', function () {
-        self.hidePhotos();
-      });
     });
-    $('#photos .back-button').click(function () {
-      photoc.hidePhotos();
-    });
-  };
-  Photocapture.prototype.hidePhotos = function hidePhoto() {
-    $('#photos').hide('fold');
-    this.cockpit.sendUpdateEnabled = true;
-    Mousetrap.unbind('esc');
+
   };
   window.Cockpit.plugins.push(Photocapture);
 }(window, jQuery));
