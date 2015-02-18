175d1acf7fa0e3a159c3d8c56c79e0cfd9eaf340
codewithpassion <dominik.fretz@gmail.com>
2015-02-18 21:00:01 +1100

    moving telemetry to webcomponent

--- src/plugins/telemetry/public/js/telemetry.js
+++ src/plugins/telemetry/public/js/telemetry.js
@@ -5,23 +5,18 @@
     console.log('Loading Telemetry plugin in the browser.');
     // Instance variables
     this.cockpit = cockpit;
-    this.telemetry = {};
     this.importantTelemetry = {};
     this.textcolor = 0;
     // Add required UI elements
-    $('html /deep/ #rov_status_panel').append('<div id="telemetry" class="controller well well-small" >\t\t    <ul>\t\t       <li id="TelemetryList">\t\t       </li>\t\t    </ul>\t\t</div>');
-    $('html /deep/ #keyboardInstructions').append('<p><i>h</i> to cycle text color of telemetry</p>');
-    var self = this;
-    setInterval(function () {
-      self.displayTelemetry();
-    }, 1000);
+    $('html /deep/ #rov_status_panel').append('');
+    cockpit.extensionPoints.keyboardInstructions.append('<p><i>h</i> to cycle text color of telemetry</p>');
   };
   //This pattern will hook events in the cockpit and pull them all back
   //so that the reference to this instance is available for further processing
   Telemetry.prototype.listen = function listen() {
     var self = this;
     this.cockpit.socket.on('status', function (data) {
-      self.logStatusData(data);
+      cockpit.emit('plugin.telemetry.logData', data);
     });
 
     self.cockpit.emit('inputController.register',
@@ -29,62 +24,9 @@
         name: "telemetry.cycleTextColor",
         description: "Cycle the text color of telemetry.",
         defaults: { keyboard: 'h' },
-        down: function() { self.cycleTextColor(); }
+        down: function() { cockpit.emit('plugin.telemetry.cycleTextColor'); }
       });
-    var list = $('html /deep/ #TelemetryList');
-    self.telemetryList = list.length > 0 ? list[0] : undefined;
 
-    if (self.telemetryList) {
-      self.telemetryList.addEventListener('click', function (e) {
-        self.handleDescendantEvent(e);
-      }, true);
-    }
-  };
-  Telemetry.prototype.cycleTextColor = function() {
-    var self = this;
-
-    self.textcolor += 5;
-    if (self.textcolor > 255) {
-      self.textcolor = 0;
-    }
-    if (self.telemetryList) {
-      self.telemetryList.style.color = 'rgb(' + self.textcolor + ',' + self.textcolor + ',255)';
-    }
-  };
-  Telemetry.prototype.handleDescendantEvent = function handleDescendantEvent(e) {
-    if (e.type == 'click' && e.eventPhase == Event.CAPTURING_PHASE) {
-      var telemetry_name = e.target.innerText.split(' ')[0];
-      if (this.importantTelemetry[telemetry_name] === true) {
-        this.importantTelemetry[telemetry_name] = false;
-      } else {
-        this.importantTelemetry[telemetry_name] = true;
-      }
-    }
-  };
-  Telemetry.prototype.logStatusData = function logStatusData(data) {
-    for (var i in data) {
-      this.telemetry[i] = data[i];
-    }
-  };
-  Telemetry.prototype.displayTelemetry = function displayTelemetry() {
-    var fragment = document.createDocumentFragment();
-    for (var item in this.telemetry) {
-      if (this.telemetry.hasOwnProperty(item)) {
-        var li = document.createElement('LI');
-        li.appendChild(document.createElement('SPAN').appendChild(document.createTextNode(item + ' ')));
-        li.appendChild(document.createElement('SPAN').appendChild(document.createTextNode(this.telemetry[item])));
-        //if (this.importantTelemetry.hasOwnProperty(item)) {
-        if (this.importantTelemetry[item] === true) {
-          li.setAttribute('class', 'important');
-        }
-        fragment.appendChild(li);
-      }
-    }
-    var list = $('html /deep/ #TelemetryList');
-    list.empty();
-    if (list.length > 0) {
-      list[0].appendChild(fragment.cloneNode(true));
-    }
   };
   window.Cockpit.plugins.push(Telemetry);
 }(window, jQuery));
