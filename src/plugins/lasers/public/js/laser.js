197f0cc316209740910583e0a4fdef461041d290
codewithpassion <dominik.fretz@gmail.com>
2015-03-04 16:46:18 +1100

    Adding laser toggle to heads up menu again

--- src/plugins/lasers/public/js/laser.js
+++ src/plugins/lasers/public/js/laser.js
@@ -5,6 +5,12 @@
     var self = this;
     self.cockpit = cockpit;
 
+  };
+
+  //This pattern will hook events in the cockpit and pull them all back
+  //so that the reference to this instance is available for further processing
+  plugins.Laser.prototype.listen = function listen() {
+    var self = this;
     self.cockpit.extensionPoints.inputController.register(
       [
         {
@@ -16,11 +22,15 @@
           }
         }
       ]);
-  };
 
-  //This pattern will hook events in the cockpit and pull them all back
-  //so that the reference to this instance is available for further processing
-  plugins.Laser.prototype.listen = function listen() {
+    if (self.cockpit.extensionPoints.headsUpMenu) {
+      self.cockpit.extensionPoints.headsUpMenu.register({
+        label: 'Toggle lasers',
+        callback: function () {
+          cockpit.rov.emit('plugin.laser.toggle');
+        }
+      });
+    }
   };
 
   window.Cockpit.plugins.push(plugins.Laser);
