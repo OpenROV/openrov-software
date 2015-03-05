5c37fddb23ff1e7d81197dcc1a62962043f8690d
codewithpassion <dominik.fretz@gmail.com>
2015-03-06 09:19:24 +1100

    Extending test

--- src/plugins/example/tests/index.js
+++ src/plugins/example/tests/index.js
@@ -10,10 +10,14 @@ var deps = {
 describe('Example plugin constructor', function() {
   beforeEach(function () {
     sinon.spy(console, 'log');
+    sinon.spy(deps.cockpit, 'on');
+    sinon.spy(deps.rov, 'registerPassthrough');
   });
 
   afterEach(function () {
     console.log.restore();
+    deps.cockpit.on.restore();
+    deps.rov.registerPassthrough.restore();
   });
 
   it('the example calls console.log', function () {
@@ -26,5 +30,15 @@ describe('Example plugin constructor', function() {
     new testee('', deps);
     console.log.calledOnce.should.be.true;
     console.log.firstCall.calledWith(sinon.match(/^This/)).should.be.ok;
+  });
+
+  it('registers on events from cockpit', function() {
+    new testee('', deps);
+    deps.cockpit.on.calledOnce.should.be.true;
+  });
+
+  it('registers passthrough commands', function() {
+    new testee('', deps);
+    deps.rov.registerPassthrough.calledOnce.should.be.true;
   });
 });
